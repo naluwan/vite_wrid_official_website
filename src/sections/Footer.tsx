c5dce1b78f8e872078f4f396ca0c628194bb74d4
naluwan <street.baller06@gmail.com>
2023-09-10 14:35:23 +0800

    feat: update Footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='text-blacks flex flex-col items-center justify-center gap-0 bg-white p-5 text-sm leading-5 dark:bg-black dark:text-white'>
+      <p className='m-0'>吾境設計股份有限公司 版權所有</p>
+      <p className='m-0'>Copyright © WRID Design Co. Ltd. All rights reserved.</p>
+      <p className='m-0'>Powered by NaLuWan</p>
+    </div>
+  );
 };
 
 export default Footer;
