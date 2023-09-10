3619551bce76a188833b87a1d56445e678895322
naluwan <street.baller06@gmail.com>
2023-09-10 13:41:12 +0800

    feat: add homeImages data

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,3 +5,54 @@ export const navLinks = [
   { href: '/collections', label: '作品欣賞' },
   { href: '/contact', label: '聯絡我們' },
 ];
+
+export const homeImages = [
+  {
+    label: '111建案',
+    imgSrc: 'https://i.imgur.com/IvUReHe.jpg',
+  },
+  {
+    label: '222建案',
+    imgSrc: 'https://i.imgur.com/c9YGkC0.jpg',
+  },
+  {
+    label: '333建案',
+    imgSrc: 'https://i.imgur.com/KU0e3QA.jpg',
+  },
+  {
+    label: '444建案',
+    imgSrc: 'https://i.imgur.com/s4O2aqI.jpg',
+  },
+  {
+    label: '555建案',
+    imgSrc: 'https://i.imgur.com/bIlIASe.jpg',
+  },
+  {
+    label: '666建案',
+    imgSrc: 'https://i.imgur.com/IMAUULI.jpg',
+  },
+  {
+    label: '777建案',
+    imgSrc: 'https://i.imgur.com/hYADneN.jpg',
+  },
+  {
+    label: '888建案',
+    imgSrc: 'https://i.imgur.com/ODZQSYp.jpg',
+  },
+  {
+    label: '999建案',
+    imgSrc: 'https://i.imgur.com/59T4UhF.jpg',
+  },
+  {
+    label: '101010建案',
+    imgSrc: 'https://i.imgur.com/IvUReHe.jpg',
+  },
+  {
+    label: '111111建案',
+    imgSrc: 'https://i.imgur.com/c9YGkC0.jpg',
+  },
+  {
+    label: '121212建案',
+    imgSrc: 'https://i.imgur.com/KU0e3QA.jpg',
+  },
+];
