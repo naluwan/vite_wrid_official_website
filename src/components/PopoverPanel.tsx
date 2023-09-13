b4f5b049007c35125241ab5d01569ef7af950083
naluwan <street.baller06@gmail.com>
2023-09-13 23:03:15 +0800

    feat: update PopoverPanel layout style and animation

--- src/components/PopoverPanel.tsx
+++ src/components/PopoverPanel.tsx
@@ -10,8 +10,8 @@ const PopoverPanel = React.forwardRef<HTMLDivElement, PopoverPanelProps>((props,
 
   return (
     <div
-      className={`popover-panel duration-5000 transition ease-in-out ${
-        openPanel ? 'block' : 'hidden'
+      className={`popover-panel origin-top transition duration-300 ease-in-out ${
+        openPanel ? 'scale-y-100 opacity-100' : 'scale-y-0 opacity-0'
       }`}
       ref={ref}
     >
