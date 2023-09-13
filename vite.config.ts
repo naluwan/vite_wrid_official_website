6b76c998378000ad7c30175d8c5070ba3920a158
naluwan <street.baller06@gmail.com>
2023-09-14 07:50:53 +0800

    feat: install vite-plugin-imagemin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'vite_wrid_official_website',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
