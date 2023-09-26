f0cfa9de31845a4855b96cf1da4c2c0060e08bba
naluwan <street.baller06@gmail.com>
2023-09-26 12:10:12 +0800

    feat: update hover shadow style on dark mode

--- src/pages/WorkFlow/components/WorkFlowCard.tsx
+++ src/pages/WorkFlow/components/WorkFlowCard.tsx
@@ -10,7 +10,7 @@ type ProcessCardProps = {
 const WorkFlowCard: React.FC<ProcessCardProps> = (props) => {
   const { icon, title, content } = props;
   return (
-    <Card className='border-none shadow-none transition-all duration-700 hover:shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] dark:bg-black dark:hover:shadow-[3.0px_8.0px_8.0px_#d1d1d15f]'>
+    <Card className='border-none shadow-none transition-all duration-700 hover:shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] dark:bg-black dark:hover:shadow-[4.0px_8.0px_8.0px_white]'>
       <CardHeader className='flex items-center'>
         <CardTitle>{icon}</CardTitle>
         <CardDescription className='pt-4 font-NotoSerif text-2xl'>
