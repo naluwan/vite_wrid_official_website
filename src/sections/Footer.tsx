4854119155d805469c9fcad0626842488c13a861
naluwan <street.baller06@gmail.com>
2023-09-26 13:04:51 +0800

    feat: update footer style

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <div className='text-blacks absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center gap-0 bg-white p-5 text-sm leading-5 dark:bg-black dark:text-white'>
+    <div className='text-blacks absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center gap-0 bg-white p-5 text-sm leading-5 dark:bg-black dark:text-white max-md:text-xs'>
       <p className='m-0'>吾境設計股份有限公司 版權所有</p>
-      <p className='m-0'>Copyright © WRID Design Co. Ltd. All rights reserved.</p>
+      <p className='m-0'>Copyright 2023 © WRID Design Co. Ltd. All rights reserved.</p>
       <p className='m-0'>Powered by NaLuWan</p>
     </div>
   );
